588f2d2ca3f7ba0536c6e13955005a85bfc41bc7
Sureshdeepthi11 <147971465+Sureshdeepthi11@users.noreply.github.com>
2023-10-15 15:14:53 +0530

    Add files via upload

---
+++ Lab015.py
@@ -0,0 +1,9 @@
+print('C:\some\name')
+print("C:\some\name")
+print(r'C:some\name')
+print('some\name')
+print('suresh\name')
+print("suresh\name")
+print(r"suresh\name")
+random_string = "iam working in bosch"
+print(len(random_string))
