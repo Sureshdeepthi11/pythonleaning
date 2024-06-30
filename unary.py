a873801739d63809df2e4bd062cfc79334a23ed4
Sureshdeepthi11 <147971465+Sureshdeepthi11@users.noreply.github.com>
2024-06-30 17:35:47 +0530

    Add files via upload

---
+++ unary.py
@@ -0,0 +1,14 @@
+#unary operaators means like + and _like opertors
+# suppose below one example
+'''a = 15
+print(a)
+print(-a)'''
+# ternary operator
+x = 5
+y = 15
+max_value = x if x>y else y
+print(max_value)
+age = input("Enter your age\n")
+age = int(age)
+result = "we can vote" if age > 18  else "we can't vote"
+print(result)
