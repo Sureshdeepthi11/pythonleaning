8aae9dfd2dfeed244a164362325bddd275cf30e1
Sureshdeepthi11 <147971465+Sureshdeepthi11@users.noreply.github.com>
2024-06-30 10:54:39 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+'''my_purchase_list = ["Real me phone","charger","shoes","spects","Car"]
+#print(my_shopping_list)
+#print(len(my_shopping_list))
+print(my_purchase_list[0:4])
+print(type(my_purchase_list))
+print(len(my_purchase_list))'''
+
+my_task_list = ["need to finish task","task2","read book","practice sessions"]
+print(my_task_list)
+print(type(my_task_list))
