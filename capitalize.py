461b7ef889555f12304b1195da9fe865f62aa381
Sureshdeepthi11 <147971465+Sureshdeepthi11@users.noreply.github.com>
2024-06-30 17:38:11 +0530

    Add files via upload

---
+++ capitalize.py
@@ -0,0 +1,12 @@
+
+name = " SuReshDeeptHi"
+result = name .capitalize()
+print(result)
+
+r1 = name.upper()
+print(r1)
+print(id(name))
+print(id(r1))
+r3 = name.lower()
+print(r3)
+print(name.swapcase())
